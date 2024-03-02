2647a31e79c16d94e56a814cdeb2370aab63555e
Furkan IŞIKAY <iletisim@furkanisikay.com.tr>
2024-03-02 22:55:18 +0300

    Update kurulum.sh
    kurulum içerisine yeniden başlatmada otomatik çalıştırılması için gerekli dosya komutları eklendi.

--- kurulum.sh
+++ kurulum.sh
@@ -35,6 +35,46 @@ sudo ninja -C build install
 echo "Kullanıcı 'dialout' grubuna ekleniyor..."
 sudo usermod -a -G dialout $USER
 
+# Kullanıcıdan yönlendirmeyi etkinleştirmek isteyip istemediğini sor
+echo "Kurulum sonrası programı her yeniden başlatmada belirlediğiniz yapılandırma bilgileriyle otomatik olarak çalıştırmak ister misiniz? (Y/N)"
+read -r cevap
+
+if [[ $cevap =~ ^[Yy]$ ]]; then
+
+  # Gerekli dizinleri ve dosyaları oluştur
+  sudo mkdir /etc/mavlink-router
+  cd /etc/mavlink-router
+  sudo touch main.conf
+
+  # Yapılandırma dosyasına gerekli bilgileri ekle
+  echo "[General]" >> main.conf
+  echo "TcpServerPort=5760" >> main.conf
+  echo "ReportStats=false" >> main.conf
+  echo "MavlinkDialect=common" >> main.conf
+  echo "" >> main.conf
+
+  echo "[UartEndpoint alpha]" >> main.conf
+  echo "Device=/dev/ttyACM0" >> main.conf
+  echo "Baud=115200" >> main.conf
+  echo "" >> main.conf
+
+  echo "[UdpEndpoint bravo]" >> main.conf
+  echo "Mode = Server" >> main.conf
+  echo "Address = 0.0.0.0" >> main.conf
+  echo "Port = 14550" >> main.conf
+
+  # Sistem servisini oluştur ve etkinleştir
+  sudo systemctl enable mavlink-router.service
+  sudo systemctl start mavlink-router.service
+
+  echo "Yönlendirme başarıyla otomatik hale getirildi!"
+
+else
+
+  echo "Yönlendirme otomatik hale getirilmedi."
+
+fi
+
 # Kurulum tamamlandı mesajı
 echo "Mavlink-router kurulumu tamamlandı!"
 echo "Kurulumu test etmek için aşağıdaki komutu çalıştırabilirsiniz:"
